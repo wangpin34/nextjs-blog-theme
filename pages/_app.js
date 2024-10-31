36944a3a23fd3723fdb9fde4a36bae31717eb6e9
Penn Wang <guyusay@gmail.com>
2024-10-31 13:51:46 +0800

    refactor: migrate next to v14.2.16

--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,16 @@
-import '../styles/globals.css';
+import Head from 'next/head';
 import 'prismjs/themes/prism-tomorrow.css';
+import '../styles/globals.css';
 
 function MyApp({ Component, pageProps }) {
   return (
     <>
+      <Head>
+        <meta
+          name="viewport"
+          content="width=device-width, initial-scale=1.0"
+        ></meta>
+      </Head>
       <span className="theme-bejamas" />
       <Component {...pageProps} />
     </>
--- pages/_document.js
+++ pages/_document.js
@@ -5,12 +5,7 @@ class MyDocument extends Document {
   render() {
     return (
       <Html lang="en" className="theme-compiled">
-        <Head>
-          <meta
-            name="viewport"
-            content="width=device-width, initial-scale=1.0"
-          ></meta>
-        </Head>
+        <Head />
         <body
           className={`antialiased text-lg bg-white dark:bg-gray-900 dark:text-white leading-base`}
         >
